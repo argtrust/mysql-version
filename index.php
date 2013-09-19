01e6a9c978b00bfa71cb2382a7c4ddf4be652372
Jordan Salvit <jordan@jordansalvit.com>
2013-09-19 00:21:59 -0400

    google analytics to index page

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
 <head>
 <title>ArgTrust Decision Making System</title>
 <meta charset="utf-8">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-43820596-1', 'trust.jordansalvit.com');
+  ga('send', 'pageview');
+
+</script>
 
 <link href="./jQuery-contextMenu-master/src/jquery.contextMenu.css" rel="stylesheet" type="text/css" />
 <link href="./jQuery-contextMenu-master/prettify/prettify.sunburst.css" rel="stylesheet" type="text/css" />
