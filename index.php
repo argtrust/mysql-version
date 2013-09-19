46deae6f2b9655f967cb0085c6d90074cae51010
Jordan Salvit <jordan@jordansalvit.com>
2013-09-19 00:23:42 -0400

    google analytics to index page

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-43820596-1', 'trust.jordansalvit.com');
+  ga('create', 'UA-43820596-1', 'jordansalvit.com');
   ga('send', 'pageview');
 
 </script>
